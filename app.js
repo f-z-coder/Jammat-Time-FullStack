6d69f86d20c40d2efa2f251236cc729101ca5303
Faiz Shaikh <faiz.22010206@viit.ac.in>
2023-07-30 22:40:58 +0530

    Add Frontend Build And handle route for client router

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const namazTimeRouter = require("./Routes/namazTimeRouter");
-const cors = require("cors");
 const connectToDatabase = require("./Database/Utility_Functions/connectToDatabase");
 
+const port = process.env.port || 8080;
+
 const app = express();
+
 connectToDatabase();
 
-app.use(cors());
+app.use(express.static(path.join(__dirname, "/Public")));
 app.use(express.json());
 app.use("/api/v1/namazTime", namazTimeRouter);
 
-app.listen(80, () => {
-  console.log("server listening");
+//these routes handle by client router
+app.get("*", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "/Public/index.html"));
+});
+app.listen(port, () => {
+  console.log("server started on port", port);
 });
